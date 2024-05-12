f8a459cbeff4bf16cefe9845cd9878b5c145c403
StevenUST <zxieam@connect.ust.hk>
2024-05-12 18:06:17 +0800

    kjsfalkj

---
+++ actor_n/utils/status.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def get_cooporate_vector(remaining : List) -> List:
+    pass
+
+def get_dwarf_vector(remaining : List) -> List:
+    pass
+
+def get_assist_vector(remaining : List) -> List:
+    pass
